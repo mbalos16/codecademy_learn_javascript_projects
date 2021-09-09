869ddc62dab634432d2c4dff3dbfc64a6fd72074
Maria Balos <mariabalos16@gmail.com>
2021-09-09 15:22:00 +0100

    Fix the vowels to duplicate just once

--- whale_talk.js
+++ whale_talk.js
@@ -7,7 +7,7 @@ Once we have converted text to the whale language, the result is sung slowly, as
  */
 
 //Write here the text you´ll like to translate into "whalw talk":
-let input = "Apunta a la luna y si fallas al menos estarás entre las estrellas";
+let input = "the cat is on the mat";
 
 // We create an array with the vowels, excluding 'y':
 const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -18,10 +18,8 @@ const resultArray =[];
 //We create a for loop that will iterate through each letter on the input variable text:
 for(let i=0; i<input.length; i++){
     //we check if the for loop works: console.log(input[i]);
-    for(let j=0; j<vowels.length; j++){
-      //we check if the for loop for j works: console.log('j is '+ j);
 
-      /*
+         /*
       we check if the input is 'e' or 'u' so we can push it to the resultArray
       because whales double this letters:
        */
@@ -29,6 +27,8 @@ for(let i=0; i<input.length; i++){
           resultArray.push(input[i]);
       }
 
+    for(let j=0; j<vowels.length; j++){
+      //we check if the for loop for j works: console.log('j is '+ j);
       if(input[i] === vowels[j]){
         resultArray.push(input[i]);
       }
